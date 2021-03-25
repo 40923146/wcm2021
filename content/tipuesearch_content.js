a13880a4cd81faa34617570fd2d8dbb9550e339b
40923146 <40923146@gm.nfu.edu.tw>
2021-03-25 11:56:14 +0800

    xx

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate \n', 'tags': '', 'url': 'About.html'}, {'title': 'HOME', 'text': 'github\xa0 \xa0 https://github.com/40923146/wcm2021 \n 1111', 'tags': '', 'url': 'HOME.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate \n', 'tags': '', 'url': 'About.html'}, {'title': 'HOME', 'text': 'github\xa0 \xa0 https://github.com/40923146/wcm2021 \n 111111', 'tags': '', 'url': 'HOME.html'}]};
